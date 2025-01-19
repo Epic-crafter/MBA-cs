6b5050ba7dbb2c402afe4cad926efe0f24042a73
Delta Student <kushkumar53021@gmail.com>
2025-01-19 17:31:05 +0530

    aboutus route added

---
+++ src/app/about/Campus.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Campus = () => {
+  return (
+    <div>
+        <h2 className='text-[30px] text-center my-10 text-[#012060] font-[700]'>CAMPUS</h2>
+        <div className='flex flex-col gap-3 justify-center items-center'>
+            <div className='flex md:flex-row flex-col gap-3'>
+                <img src="/img/aboutus/campus/Rectangle 7.png" alt="" />
+                <img src="/img/aboutus/campus/Rectangle 8.png" alt="" />
+                <img src="/img/aboutus/campus/Rectangle 9.png" alt="" />
+            </div>
+            <div className='flex md:flex-row gap-3 flex-col mb-10'>
+            <img src="/img/aboutus/campus/Rectangle 7.png" alt="" />
+                <img src="/img/aboutus/campus/Rectangle 8.png" alt="" />
+                <img src="/img/aboutus/campus/Rectangle 9.png" alt="" />
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Campus
