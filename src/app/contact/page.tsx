333b09314d78173ed0c706c990c1b93cfafc27cb
kush <kushkumar53021@gmail.com>
2025-02-06 14:10:16 +0530

    Contact us page added

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import HeroContact from './HeroContact'
+import TalkWithUs from './TalkWithUs'
+
+const page = () => {
+  return (
+    <div>
+      <HeroContact />
+      <TalkWithUs />
+    </div>
+  )
+}
+
+export default page
